98a0efd08ede550d02fc3c80bc21f3a8ea9f1b4f
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-12-06 16:49:02 -0500

    Add basename to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/frontend-mentor-space-tourism-multipage-website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
