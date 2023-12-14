b27d89549d32765854d36523f37a7bba023587ef
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-12-14 15:58:32 -0500

    Add typography utility classes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
 function Home() {
   return (
     <>
-      <h2>So you want to travel to</h2>
-      <h3>Space</h3>
-      <p>
+      <h2 className="ff-sans-condensed fs-med letter-spacing-4-72 uppercase">
+        So, you want to travel to
+      </h2>
+      <h3 className="ff-serif-regular fs-3xl uppercase">Space</h3>
+      <p className="ff-sans-regular fs-regular line-height-1-5">
         Let's face it; if you want to go to space, you might as well genuinely
         go to outer space and not hover kind of on the edge of it. Well sit
         back, and relax because we'll give you a truly out of this world
         experience!
       </p>
-      <div>Explore</div>
+      <div className="ff-serif-regular fs-lg letter-spacing-2 uppercase">
+        Explore
+      </div>
     </>
   );
 }
