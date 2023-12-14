1791aaf9da9a5cc73468514802b70621bba41560
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-12-14 15:58:43 -0500

    Add typography utility classes

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -3,8 +3,8 @@ import { Tabs, TabList, Tab, TabPanel } from 'react-aria-components';
 function Crew() {
   return (
     <>
-      <h2>
-        <span>02</span> Meet Your Crew
+      <h2 className="ff-sans-condensed fs-med letter-spacing-4-72 uppercase">
+        <span className="bold">02</span> Meet Your Crew
       </h2>
       <Tabs>
         <TabList aria-label="Technology">
@@ -14,18 +14,22 @@ function Crew() {
           <Tab id="4">4</Tab>
         </TabList>
         <TabPanel id="1">
-          <h3>Commander</h3>
-          <h4>Douglas Hurley</h4>
-          <p>
+          <h3 className="ff-serif-regular fs-lg uppercase">Commander</h3>
+          <h4 className="ff-serif-regular fs-xl uppercase">Douglas Hurley</h4>
+          <p className="ff-sans-regular fs-regular line-height-1-5">
             Douglas Gerald Hurley is an American engineer, former Marine Corps
             pilot and former NASA astronaut. He launched into space for the
             third time as commander of Crew Dragon Demo-2.
           </p>
         </TabPanel>
         <TabPanel id="2">
-          <h3>Mission Specialist</h3>
-          <h4>Mark Shuttleworth</h4>
-          <p>
+          <h3 className="ff-serif-regular fs-lg uppercase">
+            Mission Specialist
+          </h3>
+          <h4 className="ff-serif-regular fs-xl uppercase">
+            Mark Shuttleworth
+          </h4>
+          <p className="ff-sans-regular fs-regular line-height-1-5">
             Mark Richard Shuttleworth is the founder and CEO of Canonical, the
             company behind the Linux-based Ubuntu operating system. Shuttleworth
             became the first South African to travel to space as a space
@@ -33,9 +37,9 @@ function Crew() {
           </p>
         </TabPanel>
         <TabPanel id="3">
-          <h3>Pilot</h3>
-          <h4>Victor Glover</h4>
-          <p>
+          <h3 className="ff-serif-regular fs-lg uppercase">Pilot</h3>
+          <h4 className="ff-serif-regular fs-xl uppercase">Victor Glover</h4>
+          <p className="ff-sans-regular fs-regular line-height-1-5">
             Pilot on the first operational flight of the SpaceX Crew Dragon to
             the International Space Station. Glover is a commander in the U.S.
             Navy where he pilots an F/A-18.He was a crew member of Expedition
@@ -43,9 +47,9 @@ function Crew() {
           </p>
         </TabPanel>
         <TabPanel id="4">
-          <h3>Flight Engineer</h3>
-          <h4>Anousheh Ansairi</h4>
-          <p>
+          <h3 className="ff-serif-regular fs-lg uppercase">Flight Engineer</h3>
+          <h4 className="ff-serif-regular fs-xl uppercase">Anousheh Ansairi</h4>
+          <p className="ff-sans-regular fs-regular line-height-1-5">
             Anousheh Ansari is an Iranian American engineer and co-founder of
             Prodea Systems. Ansari was the fourth self-funded space tourist, the
             first self-funded woman to fly to the ISS, and the first Iranian in
