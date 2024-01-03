9cb5e284e94749bad79f7d64bd85caebae54d3d8
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2024-01-03 13:08:43 -0500

    Add Link to Logo; add <> around Nav; add classname="nav" to Links; remove utility classes

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,31 +4,35 @@ import Logo from '../assets/img/shared/logo.svg';
 
 function Nav() {
   return (
-    <nav>
-      <img src={Logo} alt="Logo" />
-      <ul className="ff-sans-condensed fs-sm letter-spacing-2-7 uppercase text-white">
-        <li>
-          <Link to="/">
-            <span className="bold">00</span> Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/destination">
-            <span className="bold">01</span> Destination
-          </Link>
-        </li>
-        <li>
-          <Link to="/crew">
-            <span className="bold">02</span> Crew
-          </Link>
-        </li>
-        <li>
-          <Link to="technology">
-            <span className="bold">03</span> Technology
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <Link to="/" className="nav">
+          <img src={Logo} alt="Logo" />
+        </Link>
+        <ul>
+          <li>
+            <Link to="/" className="nav">
+              <span className="bold">00</span> Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/destination" className="nav">
+              <span className="bold">01</span> Destination
+            </Link>
+          </li>
+          <li>
+            <Link to="/crew" className="nav">
+              <span className="bold">02</span> Crew
+            </Link>
+          </li>
+          <li>
+            <Link to="technology" className="nav">
+              <span className="bold">03</span> Technology
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 }
 
