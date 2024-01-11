8e7b8fbfd6d780a4956ef19c7a8b4da902830d7e
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2024-01-11 12:58:02 -0500

    Change Nav to Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 // Components
-import Nav from './components/Nav';
+import Header from './components/Header';
 // Pages
 import Home from './pages/Home';
 import Destination from './pages/Destination';
@@ -10,15 +10,15 @@ import Technology from './pages/Technology';
 
 function App() {
   return (
-    <div>
-      <Nav />
+    <>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
         <Route path="/crew" element={<Crew />} />
         <Route path="/technology" element={<Technology />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
