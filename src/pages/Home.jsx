dc3b1289271ab9ec995e7d0c0c84dd72803cccf8
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2024-01-19 15:11:34 -0500

    Add sections and divs for grid container

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 function Home() {
   return (
     <>
-      <h2 className="ff-sans-condensed fs-med letter-spacing-4-72 uppercase text-light">
-        So, you want to travel to
-      </h2>
-      <h3 className="ff-serif-regular fs-3xl uppercase text-white">Space</h3>
-      <p className="ff-sans-regular fs-regular line-height-1-5 text-light">
-        Let's face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we'll give you a truly out of this world
-        experience!
-      </p>
-      <button className="lg-button">Explore</button>
+      <section className="grid grid__container">
+        <div className="grid__container-item grid__container-item-1">
+          <h2 className="ff-sans-condensed fs-med line-height-1-5 letter-spacing-4-72 uppercase text-light">
+            So, you want to travel to
+          </h2>
+          <h3 className="ff-serif-regular fs-3xl line-height-1-5 uppercase text-white">
+            Space
+          </h3>
+          <p className="ff-sans-regular fs-regular line-height-1-5 text-light">
+            Let's face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we'll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="grid__container-item grid__container-item-2">
+          <button className="lg-button">Explore</button>
+        </div>
+      </section>
     </>
   );
 }
